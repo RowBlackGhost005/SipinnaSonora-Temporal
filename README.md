99ccd5c17d64791ba97fd1b5eec4bb045019e4d3
Luis Angel Marin <la.marin@outlook.com>
2024-03-05 12:48:48 -0700

    Xls Parser
    Agregado lector de archivos XLS

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SipinnaSonora-Temporal
 Sipina Sonora Temporal Repo BACK
+
+Libraries Required:
+dotnet add package Aspose.Cells --version 24.2.0
---
+++ apiSipinna/Modules/XlsParser/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Numerics;
+using Excel_Parser_Test;
+using ExcelDataReader;
+
+//Requiered to work with ReaderClass
+// Required in a MultiPart JSON/POST while in API.
+// Then convert to FileStream
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+String filepath = "apiSipinna/Assests/Test Formato.xlsx";
+FileStream stream = File.Open(filepath, FileMode.Open, FileAccess.Read);
+///
+
+XlsParser xlsParser = new XlsParser(stream);
+
+//Fetching data XLS Data
+
+/*
+Console.WriteLine(xlsParser.GetStatisticName());
+Console.WriteLine(xlsParser.GetStatisticDate());
+Console.WriteLine(xlsParser.GetStatisticScope());
+foreach (var item in xlsParser.GetEntitiesNames())
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in xlsParser.GetAgeRanges())
+{
+    Console.WriteLine(item);
+}
+
+foreach(var item in xlsParser.GetPopulations())
+{
+    Console.WriteLine(item);
+}
+*/
+
+
+foreach(var item in xlsParser.GetStatisticData())
+{
+    Console.WriteLine(item);
+}

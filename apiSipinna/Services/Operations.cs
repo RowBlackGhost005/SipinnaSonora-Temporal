08c0a1826e7d2cb034b47a3b1d10f3ceb7bdca89
Ignacio Simons <ignacio.smns@gmail.com>
2024-03-06 11:38:42 -0700

    Insert-p3

--- apiSipinna/Services/Operations.cs
+++ apiSipinna/Services/Operations.cs
@@ -39,6 +39,46 @@ namespace apiSipinna.CRUD{
             }
         }
 
+        public Task<bool> DeleteCategoria(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCobertura(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteEdades(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteEstadistica(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFecha(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFecha(Fecha fecha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteLugar(Lugar lugar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteLugar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<Categoria> ReadCategoria(int id){
             if(id<=0){
                 throw new Exception("'id' no puede ser igual o menor que '0'");
@@ -47,6 +87,60 @@ namespace apiSipinna.CRUD{
             }
         }
 
+        public Task<Categoria> ReadCobertura(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Edades> ReadEdades(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Estadistica> ReadEstadistica(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fecha> ReadFecha(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Lugar> ReadLugar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategoria(Categoria cat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCobertura(Categoria cat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateEdades(Edades edades)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateEstadistica(Estadistica estadistica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateFecha(Fecha fecha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateLugar(Lugar lugar)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
